3184bd138a8bf9bf55ad9cd64b1ea5606e8e9c42
Ryan Williams <ryan.blake.williams@gmail.com>
2017-08-22 19:58:58 +0000

    use Show for Split diffs in compare-splits

--- src/main/scala/org/hammerlab/bam/spark/compare/Main.scala
+++ src/main/scala/org/hammerlab/bam/spark/compare/Main.scala
@@ -158,9 +158,9 @@ object Main
             diffs
               .map {
                 case Left(ours) ⇒
-                  s"\t\t$ours"
+                  show"\t\t$ours"
                 case Right(theirs) ⇒
-                  s"\t\t\t$theirs"
+                  show"\t\t\t$theirs"
               },
             s"\t${path.basename}: ${diffs.length} splits differ ($totalsMsg):",
             n ⇒ s"\t${path.basename}: first $n of ${diffs.length} splits that differ ($totalsMsg):"
